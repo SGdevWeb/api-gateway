7cbbe9528e42585ec78cf03babcb080779367e68
Emmanuel <caronnunu@gmail.com>
2023-02-24 16:10:17 +0100

    Sync code http request on route create project

--- controller/projectController.js
+++ controller/projectController.js
@@ -4,9 +4,9 @@ require('dotenv').config();
 const create = async (req, res) => {
     try {
         const response = await axios.post(process.env.PROJECT_SERVICE_ADDRESS + "/project/create", { ...req.body, user: req.auth });
-        return res.status(201).send(response.data.success);
+        return res.status(response.status).send(response.data.success);
     } catch (error) {
-        return res.status(500).json({ message: error });
+        return res.status(error.response ? error.response.status : 500).json({ message: error });
     }
 };
 
