d6553cce0000593cbed354d3c8aa44dedab70b50
Emmanuel <caronnunu@gmail.com>
2023-02-27 16:17:17 +0100

    change message error for create project

--- controller/projectController.js
+++ controller/projectController.js
@@ -6,7 +6,7 @@ const create = async (req, res) => {
         const response = await axios.post(process.env.PROJECT_SERVICE_ADDRESS + "/project/create", { ...req.body, user: req.auth });
         return res.status(response.status).send(response.data.success);
     } catch (error) {
-        return res.status(error.response ? error.response.status : 500).json({ message: error });
+        return res.status(error.response ? error.response.status : 500).json({ error });
     }
 };
 
