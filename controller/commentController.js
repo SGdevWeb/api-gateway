5a93d6f35df96b2b191273fd5b42ad6cf46efe90
SGdevWeb <samuel.gustin.dev@gmail.com>
2023-03-01 11:35:53 +0100

    Add updateComment and modification payload of token

--- controller/commentController.js
+++ controller/commentController.js
@@ -1,13 +1,17 @@
 const axios = require('axios')
 
 const commentPost = async (req, res) => {
-    console.log('commentPost body',req.body)
+    // console.log('commentPost body',req.body)
+    // console.log('commentPost auth',req.auth)
     try {
         const response = await axios.post(`${process.env.PROJECT_SERVICE_ADDRESS}comments/post`, {
             comment: req.body.comment,
-            uuid_user: req.body.uuid_user
+            uuid_user: req.auth.user.uuid,
+            uuid_project: req.body.uuid_project,
+            avatar: req.auth.user.avatar,
+            username: req.auth.user.username
         })
-        console.log('data', response.data)
+        // console.log('data', response.data)
         const {success} = response.data
         return res.status(201).json( success )
     } catch (error) {
@@ -16,7 +20,7 @@ const commentPost = async (req, res) => {
 }
 
 const getAllComments = async (req, res) => {
-    console.log(req.body)
+    // console.log(req.body)
     try {
         const response = await axios.get(`${process.env.PROJECT_SERVICE_ADDRESS}comments`)
         console.log('data getAllComments', response.data)
@@ -27,7 +31,23 @@ const getAllComments = async (req, res) => {
     }
 }
 
+const updateComment = async (req, res) => {
+    // console.log('body', req.body)
+    // console.log('auth', req.auth)
+    try {
+        const response = await axios.put(`${process.env.PROJECT_SERVICE_ADDRESS}comments/comment`, {
+            ...req.body, uuid_user: req.auth.user.uuid
+        })
+        // console.log('data', response.data)
+        const {success} = response.data
+        return res.status(200).json(success)
+    } catch (error) {
+        return res.status(500).json({ message: error.message })
+    }
+}
+
 module.exports = {
     commentPost,
-    getAllComments
+    getAllComments,
+    updateComment,
 }
--- route/projectRoute.js
+++ route/projectRoute.js
@@ -3,13 +3,17 @@ const router = express.Router();
 require('dotenv').config()
 const DTO = require('../dto/dtos')
 const { commentController } = require('../controller/controllers');
+const auth = require('../middleware/auth')
 
 // url route microservice project
 const COMMENTS = process.env.API_ADDRESS+"/comments";
+const COMMENT = process.env.API_ADDRESS+"/comments/comment"
 
 // route : url , dto , controller
-router.post(COMMENTS, commentController.commentPost);
+router.post(COMMENTS, auth, commentController.commentPost);
 
-router.get(COMMENTS, commentController.getAllComments)
+router.get(COMMENTS, commentController.getAllComments);
+
+router.put(COMMENT, auth, commentController.updateComment)
 
 module.exports = router;
