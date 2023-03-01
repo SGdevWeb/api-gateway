8fcace815612b414da9952ca107ee5b792a1ced9
DavidBonvin <ledave.musique@gmail.com>
2023-03-01 16:40:02 +0100

    MAJ addColl2

--- controller/collaboratorsController.js
+++ controller/collaboratorsController.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 require('dotenv').config();
 
 const create = async (req, res) => {
- console.log(req.body)
+//  console.log(req.body)
     try {
         const response = await axios.post(process.env.PROJECT_SERVICE_ADDRESS + "/collaborators/add", { ...req.body, user: req.auth });
         return res.status(response.status).send(response.data.success);
