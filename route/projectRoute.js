b3e1ef9cee401348191d9fcb7e16142f2a215f19
SGdevWeb <samuel.gustin.dev@gmail.com>
2023-03-07 09:52:21 +0100

    Add projectRoute

---
+++ route/projectRoute.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const router = express.Router();
+require("dotenv").config();
+const auth = require("../middleware/auth");
+const DTO = require("../dto/dtos");
+const Controller = require("../controller/controllers");
+
+router.post(
+  "/create",
+  auth,
+  DTO.project.create,
+  Controller.projectController.create
+);
+
+module.exports = router;
