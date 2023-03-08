04e4ea937b746dbc66060937451899e5bdf790d9
Emmanuel <caronnunu@gmail.com>
2023-03-08 16:09:54 +0100

    Add listing project  all and by user

--- controller/projectController.js
+++ controller/projectController.js
@@ -33,8 +33,28 @@ const get = async (req, res) => {
     }
 };
 
+const getAll = async (_req, res) => {
+  try {
+      const response = await axios.get(process.env.PROJECT_SERVICE_ADDRESS + "/project/");
+      return res.status(response.status).send(response.data.success);
+  } catch (error) {
+      return res.status(error.response ? error.response.status : 500).json({ message: error.response ? error.response.data : error.message });
+  }
+};
+
+const getByUser = async (req, res) => {
+  try {
+      const response = await axios.get(process.env.PROJECT_SERVICE_ADDRESS + "/project/user/" + req.params.uuid);
+      return res.status(response.status).send(response.data.success);
+  } catch (error) {
+      return res.status(error.response ? error.response.status : 500).json({ message: error.response ? error.response.data : error.message });
+  }
+};
+
 module.exports = {
     create,
     update, 
     get,
+    getAll,
+    getByUser,
 }
